9195eeac1887768043c5418252c4e2f7e5cabd9e
test <2585330848@qq.com>
2018-05-02 20:20:59 +0800

    通过应用内通知实现登录逻辑。 未完成，需要实现：调用httpUtils如果返回403，则需要跳转到登录界面。（实现方式：返回403，则发送一个intent。活动的activity会受到这个intent，进而跳转到登录页面）

--- app/src/main/java/com/example/tmac/testapp/activity/AbstractBaseActivity.java
+++ app/src/main/java/com/example/tmac/testapp/activity/AbstractBaseActivity.java
@@ -1,10 +1,18 @@
 package com.example.tmac.testapp.activity;
 
+import android.app.AlertDialog;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.widget.Toast;
+
+import com.example.tmac.testapp.utils.ActivityCollector;
 
 public class AbstractBaseActivity extends AppCompatActivity {
     public Handler handler = new Handler(){
@@ -16,6 +24,59 @@ public class AbstractBaseActivity extends AppCompatActivity {
              startActivity(i);
          };
     };
+
+    private ForceOfflineReceiver receiver;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ActivityCollector.addActivity(this);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        IntentFilter intentFilter = new IntentFilter();
+        intentFilter.addAction("com.example.broadcastbestpractice.FORCE_OFFLINE");
+        receiver = new ForceOfflineReceiver();
+        registerReceiver(receiver, intentFilter);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (receiver != null) {
+            unregisterReceiver(receiver);
+            receiver = null;
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        ActivityCollector.removeActivity(this);
+    }
+
+    class ForceOfflineReceiver extends BroadcastReceiver {
+
+        @Override
+        public void onReceive(final Context context, Intent intent) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(context);
+            builder.setTitle("Warning");
+            builder.setMessage("You are forced to be offline. Please try to login again.");
+            builder.setCancelable(false);
+            builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    ActivityCollector.finishAll(); // 销毁所有活动
+                    Intent intent = new Intent(context, com.example.tmac.testapp.activity.LoginActivity.class);
+                    context.startActivity(intent); // 重新启动LoginActivity
+                }
+            });
+            builder.show();
+        }
+
+    }
 
 
 }
