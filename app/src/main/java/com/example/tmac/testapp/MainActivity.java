c72f22306015c5ce0a0ad57d33e4415c39ab78e4
test <2585330848@qq.com>
2018-12-04 19:18:52 +0800

    1、改变tab颜色 2、主页面去掉actionBar 3、修改闪屏图片

--- app/src/main/java/com/example/tmac/testapp/MainActivity.java
+++ app/src/main/java/com/example/tmac/testapp/MainActivity.java
@@ -11,6 +11,8 @@ import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewCompat;
 import android.support.v4.view.ViewPager;
 import android.util.Log;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -47,6 +49,11 @@ public class MainActivity extends AbstractBaseActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);//remove title bar  即隐藏标题栏
+        getSupportActionBar().hide();// 隐藏ActionBar
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);//remove notification bar
+
         setContentView(R.layout.activity_main);
 
         mTabTl = (TabLayout) findViewById(R.id.tl_tab);
