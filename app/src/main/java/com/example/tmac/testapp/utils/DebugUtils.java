22a367ac72bfb0fb167d7398e14e73ec801d62a8
test <2585330848@qq.com>
2018-12-05 11:21:29 +0800

    修改bug，正式发布非debug的时候设置为调试界面不可见

--- app/src/main/java/com/example/tmac/testapp/utils/DebugUtils.java
+++ app/src/main/java/com/example/tmac/testapp/utils/DebugUtils.java
@@ -8,7 +8,7 @@ import com.example.tmac.testapp.utils.codec.KeyUtils;
 
 public class DebugUtils {
 
-    public static boolean isDebug = true;
+    public static boolean isDebug = false;
     private static String Base64PublicKey = "MFwwDQYJKoZIhvcNAQEBBQADSwAwSAJBALhHae2OxGQEqZ9+o1tBfGyi6HKlDJwcb8TeH5r7cP+IbyVcMyQOXzZUe93F0nq3Rf2FQFxSvv64tSuhyqN4GgUCAwEAAQ==";
     private static String Base64PrivateKey = "MIIBVAIBADANBgkqhkiG9w0BAQEFAASCAT4wggE6AgEAAkEAuEdp7Y7EZASpn36jW0F8bKLocqUMnBxvxN4fmvtw/4hvJVwzJA5fNlR73cXSerdF/YVAXFK+/ri1K6HKo3gaBQIDAQABAkB0/qH5+JInDe7DHBuKbGEad7MsVi1TT5qGWp+mPczX7isH2L5jS3t3pyseJRyjjurZcMxw31Uzs35ecrThEYIBAiEA3ckn4EWmeJZHC/sfuYp5/CEj98h4muyfrqVzKPmdwlECIQDUtQaPCqTxYqhFLYNUffmR3pKRKy81CItnLs7G8zPbdQIhAMFCzDCuAMYLjZVvGtH/TRTbfBAq7I1zrCHvok4OVd5BAiBnDRkTXf3RcJHDiixRWAL8d8+cg0aItA4NvHCzlIowLQIgXhCx6K1cAJB6hyjpFSqyEjEAUqTcBFzGZPw/txsyjl0=";
     private static String Host = "http://mobile.test.id.link:6060";
@@ -24,7 +24,7 @@ public class DebugUtils {
     }
 
     public static void setMfaLayoutVisibility(View contentView){
-        if(isDebug){
+        if(!isDebug){
             LinearLayout mfaDebugLayout = contentView.findViewById(R.id.mfaDebugLayout);
             mfaDebugLayout.setVisibility(View.INVISIBLE);
         }
